04b77b7de87ed361c9a4e1c07ad3f3accab3fb66
Rcm <rcm68888@gmail.com>
2024-05-01 14:03:06 -0600

    added exercise 2&3

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,17 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
+# Exercise 2: Update the first store
+# Load the first store (with id = 1) from the database and assign it to an instance variable @store1.
+# Load the second store from the database and assign it to @store2.
+# Update the first store (@store1) instance in the database. (Change its name or something.)
+
 # Your code goes here ...
+@store1 = Store.first
+@store2 = Store.second
+
+@store1.name = "Kelowna"
+@store2.name = "Abbotsford"
+
+puts @store1.name
+puts @store2.name
