94f904eef5f6d1b12265c063b529c93dee45d443
Rcm <rcm68888@gmail.com>
2024-05-01 16:30:41 -0600

    added exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,33 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
+# Exercise 7: Validations for both models
+# 1. Add validations to two models to enforce the following business rules:
+# - Employees must always have a first name present
+# - Employees must always have a last name present
+# - Employees have a hourly_rate that is a number (integer) between 40 and 200
+# - Employees must always have a store that they belong to (can't have an employee that is not assigned a store)
+# - Stores must always have a name that is a minimum of 3 characters
+# - Stores have an annual_revenue that is a number (integer) that must be 0 or more
+# - BONUS: Stores must carry at least one of the men's or women's apparel (hint: use a custom validation method - don't use a Validator class)
+# 2. Ask the user for a store name (store it in a variable)
+# 3. Attempt to create a store with the inputted name but leave out the other fields (annual_revenue, mens_apparel, and womens_apparel)
+# 4. Display the error messages provided back from ActiveRecord to the user (one on each line) after you attempt to save/create the record
+
 # Your code goes here ...
+class Employee
+  Employee.validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: {greater_than_or_equal_to: 40, less_than_or_equal_to: 200}, presence: true
+  validates :store_id, presence: true
+end
+
+class Store
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than: 0 }
+end
+
+puts "Enter a store name: "
+@store_name = gets.chomp
+
+p Store.create(name: @store_name).errors.full_messages
